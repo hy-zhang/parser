cd1269be2c092c37b55eb99687d9ceaf6f62764e
Huang Li <lihuanglx@gmail.com>
2016-08-11 22:03:28 +0800

    Simplify Arith

--- src/Arith.hs
+++ src/Arith.hs
@@ -10,7 +10,7 @@ module Arith (TmBool(..), TmNat(..), TmArith(..)) where
 
 import           Lib
 import           Text.Parsec      hiding (runP)
-import           Text.PrettyPrint hiding (char, parens, space)
+import           Text.PrettyPrint hiding (char, space)
 
 -- TmBool
 
@@ -18,9 +18,9 @@ data TmBool e = TmTrue | TmFalse | TmIf e e e deriving (Functor, Show)
 
 parseTmBool :: NewParser TmBool fs
 parseTmBool e p =
-  (string "true" >> pure (In e TmTrue)) <|>
-  (string "false" >> pure (In e TmFalse)) <|>
-  do { try (keyword "if"); e1 <- p;
+  (keyword "true" >> pure (In e TmTrue)) <|>
+  (keyword "false" >> pure (In e TmFalse)) <|>
+  do { keyword "if"; e1 <- p;
        keyword "then"; e2 <- p;
        keyword "else"; e3 <- p;
        return $ In e (TmIf e1 e2 e3)}
@@ -29,7 +29,7 @@ instance Syntax TmBool where
   parseF                    = parseTmBool
   prettyF _ TmTrue          = text "true"
   prettyF _ TmFalse         = text "false"
-  prettyF r (TmIf e1 e2 e3) = text "(if " <> r e1 <> text " then " <> r e2 <> text " else " <> r e3 <> text ")"
+  prettyF r (TmIf e1 e2 e3) = parens $ text "if " <> r e1 <> text " then " <> r e2 <> text " else " <> r e3
 
 -- TmNat
 
@@ -38,25 +38,25 @@ data TmNat e = TmZero | TmSucc e | TmPred e deriving (Functor, Show)
 parseTmNat :: NewParser TmNat fs
 parseTmNat e p =
   (char '0' >> pure (In e TmZero)) <|>
-  (keyword "succ" >> (pure (In e . TmSucc) <*> p)) <|>
-  (keyword "pred" >> (pure (In e . TmPred) <*> p))
+  (keyword "succ" >> (In e . TmSucc) <$> p) <|>
+  (keyword "pred" >> (In e . TmPred) <$> p)
 
 instance Syntax TmNat where
   parseF               = parseTmNat
   prettyF _ TmZero     = text "0"
-  prettyF r (TmSucc e) = text "succ (" <> r e <> text ")"
-  prettyF r (TmPred e) = text "pred (" <> r e <> text ")"
+  prettyF r (TmSucc e) = text "succ" <+> parens (r e)
+  prettyF r (TmPred e) = text "pred" <+> parens (r e)
 
 -- TmArith
 
 data TmArith e = TmIsZero e deriving (Functor, Show)
 
 parseTmArith :: NewParser TmArith fs
-parseTmArith e p = try (keyword "iszero") >> (pure (In e . TmIsZero) <*> p)
+parseTmArith e p = keyword "iszero" >> (In e . TmIsZero <$> p)
 
 instance Syntax TmArith where
   parseF                 = parseTmArith
-  prettyF r (TmIsZero e) = text "iszero (" <> r e <> text ")"
+  prettyF r (TmIsZero e) = text "iszero" <+> parens (r e)
 
 -- Test
 
