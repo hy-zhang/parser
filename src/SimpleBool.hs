11ae0e6ab891c90a996bd31a1ce97f0a04ea278a
Huang Li <lihuanglx@gmail.com>
2016-08-13 15:43:22 +0800

    SimpleBool

---
+++ src/SimpleBool.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+
+module SimpleBool where
+
+import           Lib
+import           Text.Parsec      hiding (runP)
+import           Text.PrettyPrint hiding (char, space)
+
+import           Arith
+import           TyArith
+import           Untyped
+
+-- TyArr
+
+data TyArr e = TyArr e e deriving (Functor, Show)
+
+parseTyArr :: NewParser TyArr fs
+parseTyArr e p = chainlR (keyword "->" >> p) TyArr e p
+
+instance Syntax TyArr where
+  parseF                    = parseTyArr
+  prettyF r (TyArr t1 t2)   = r t1 <> text "->" <> r t2
+
+
+-- New lambda
+
+data TmLam2 e = TmLam2 String e e deriving (Functor, Show)
+
+parseTmLam2 :: NewParser TmLam2 fs
+parseTmLam2 e p = do
+  keyword "\\"
+  x <- parseWord
+  keyword ":"
+  t <- p
+  keyword "."
+  body <- p
+  return $ In e (TmLam2 x t body)
+
+instance Syntax TmLam2 where
+  parseF = parseTmLam2
+  prettyF r (TmLam2 x t e) = text "\\" <> text x <> colon <> r t <> text "." <> r e
+
+-- Test
+
+s :: Syntactic '[TyArr, TmBool, TyBool, TmApp, TmLam2, TmVar]
+s = crep
+
+test :: IO ()
+test = mapM_ (runP s) [
+  "true",
+  "if false then true else false",
+  "Bool",
+  "\\x:Bool.x",
+  "Bool->Bool",
+  "(\\x:Bool->Bool.x)",
+  "(\\x:Bool->Bool.if (x false) then true else false) (\\x:Bool.if x then false else true)"]
