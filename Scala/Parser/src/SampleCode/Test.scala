2a5028f12ec98f16b3ec39f089fe98fad003e8e5
hy-zhang <zhanghaoyuan00@gmail.com>
2016-10-18 17:49:23 +0800

    Refactor code in paper.

--- Scala/Parser/src/SampleCode/Test.scala
+++ Scala/Parser/src/SampleCode/Test.scala
@@ -34,6 +34,43 @@ object Test extends StandardTokenParsers with PackratParsers {
     a
   }
   
+  def merge[E, F, G, H[_,_]](op : F => G => H[F, G], x : (=> E) => F, y : (=> E) => G)
+    : (=> E) => H[F, G] = e => op(x(e))(y(e))
+  
+  type Parser[E] = PackratParser[E]
+  type GenParser[E] = (=> Parser[E]) => Parser[E]
+  implicit class ParserCombinator[E, R](x : (=> R) => Parser[E]) {
+    type H[A, B] = Parser[E]
+    def |||(y : (=> R) => Parser[E]) : (=> R) => Parser[E] = {
+      val alt : Parser[E] => Parser[E] => Parser[E] = a => b => a ||| b
+      merge[R, Parser[E], Parser[E], H](alt, x, y)
+    }
+  }
+  
+  type Pair = (Parser[Expr], Parser[Type])
+  
+  class Type
+  class IntT extends Type
+  val pInt : (=> Pair) => Parser[Type] =
+    p => "Int" ^^ { _ => new IntT() }
+
+  class Lam(x : String, t : Type, e : Expr) extends Expr
+  val pLam : (=> Pair) => Parser[Expr] =
+    p => ("\\" ~> ident) ~ (":" ~> p._2) ~ ("." ~> p._1) ^^
+      { case x ~ t ~ e => new Lam(x, t, e) }
+    
+  
+  val pVar2 : (=> Pair) => Parser[Expr] = p => pVar(p._1)
+  val pApp2 : (=> Pair) => Parser[Expr] = p => pApp(p._1)
+
+  val pET : (=> Pair) => Pair = {
+    val pE = pVar2 ||| pApp2 ||| pLam
+    val pT = pInt
+    p => (pE(p), pT(p))
+  }
+    
+  
+  
   def main(args : Array[String]) = {
     runParser(fix(pVarApp))("x y")
   }
