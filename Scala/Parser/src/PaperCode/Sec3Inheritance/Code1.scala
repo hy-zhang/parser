b35eadd61803a7083b77a23768fa95ff8dd31309
Huang Li <lihuanglx@gmail.com>
2017-01-12 01:06:37 +0800

    Fix 3.4

--- Scala/Parser/src/PaperCode/Sec3Inheritance/Code1.scala
+++ Scala/Parser/src/PaperCode/Sec3Inheritance/Code1.scala
@@ -47,9 +47,6 @@ trait VarExprParser extends ExprParser {
 val r = parse(new VarExprParser {}.pExpr)("1 + x").print // "(1 + x)"
 //END_INHERITANCE_APPROACH
 
-  def main(args: Array[String]): Unit = {
-    println(r)
-  }
 
   // ---------------------------------------------------
 
@@ -76,4 +73,54 @@ trait LanguageC extends LanguageA with LanguageB {
 }
 //END_MULTIPLE_INHERITANCE
 */
+
+/*
+//BEGIN_BOOL_EXAMPLE
+class BoolLit(x: Boolean) extends Expr {...}
+class If(p: Expr, e1: Expr, e2: Expr) extends Expr {...}
+
+trait BoolParser {
+  lexical.reserved += ("true", "false", "if", "then", "else")
+  val pBoolLit: Parser[Expr] =
+    "true" ^^^ new BoolLit(true) ||| "false" ^^^ new BoolLit(false)
+  val pIf: Parser[Expr] = ("if" ~> pExpr) ~ ("then" ~> pExpr) ~ ("else" ~> pExpr) ^^
+    { case e1 ~ e2 ~ e3 => new If(e1, e2, e3) }
+  def pExpr: Parser[Expr] =
+    pBoolLit ||| pIf
+}
+
+trait ArithBoolParser extends ExprParser with BoolParser {
+  override def pExpr: Parser[Expr] =
+    super[ExprParser].pExpr ||| super[BoolParser].pExpr
+}
+//END_BOOL_EXAMPLE
+*/
+
+  class BoolLit(x: Boolean) extends Expr {
+    def print: String = x.toString
+  }
+  class If(p: Expr, e1: Expr, e2: Expr) extends Expr {
+    def print: String = "if " + p.print + " then " + e1.print + " else " + e2.print
+  }
+
+  trait BoolParser {
+    lexical.reserved += ("true", "false", "if", "then", "else")
+    val pBoolLit: Parser[Expr] =
+      "true" ^^^ new BoolLit(true) ||| "false" ^^^ new BoolLit(false)
+    val pIf: Parser[Expr] = ("if" ~> pExpr) ~ ("then" ~> pExpr) ~ ("else" ~> pExpr) ^^
+      { case e1 ~ e2 ~ e3 => new If(e1, e2, e3) }
+    def pExpr: Parser[Expr] =
+      pBoolLit ||| pIf
+  }
+
+  trait ArithBoolParser extends ExprParser with BoolParser {
+    override def pExpr: Parser[Expr] =
+      super[ExprParser].pExpr ||| super[BoolParser].pExpr
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(r)
+    val r2 = parse(new ArithBoolParser {}.pExpr)("if true then 1 + 2 else 3 + 4").print
+    println(r2)
+  }
 }
