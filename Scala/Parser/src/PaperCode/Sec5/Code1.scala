bccd45860809a4d097201617dcaca6c3208e7562
Huang Li <lihuanglx@gmail.com>
2017-01-12 17:54:09 +0800

    Revised 5.1

--- Scala/Parser/src/PaperCode/Sec5/Code1.scala
+++ Scala/Parser/src/PaperCode/Sec5/Code1.scala
@@ -1,7 +1,76 @@
 package PaperCode.Sec5
 
+import PaperCode.Common
 
-object Code1 {
+
+object Code1 extends Common {
+  trait Alg[E] {
+    def varE(x: String): E
+
+    def lam(x: String, e: E): E
+  }
+
+  trait ExtAlg[E, T] extends Alg[E] {
+    def tyInt(): T
+
+    def tyLam(x: String, t: T, e: E): E
+  }
+
+  trait Print extends ExtAlg[String, String] {
+    override def varE(x: String) = x
+
+    override def lam(x: String, e: String) = "\\" + x + "." + e
+
+    override def tyInt() = "Int"
+
+    override def tyLam(x: String, t: String, e: String) = "\\" + x + ":" + t + "." + e
+  }
+
+  trait BaseParser[E] {
+    lexical.delimiters += ("\\", ".")
+    val alg: Alg[E]
+    val pVar: Parser[E] = ident ^^ alg.varE
+    lazy val pLam: Parser[E] = ("\\" ~> ident) ~ ("." ~> pE) ^^ { case x ~ e => alg.lam(x, e) }
+    val pE: Parser[E] = pLam ||| pVar
+  }
+
+  trait ExtParser[E, T] extends BaseParser[E] {
+    lexical.delimiters += ("\\", ":", ".")
+    lexical.reserved += "Int"
+    override val alg: ExtAlg[E, T]
+    override lazy val pLam: Parser[E] = ("\\" ~> ident) ~ (":" ~> pT) ~ ("." ~> pE) ^^ { case x ~ t ~ e => alg.tyLam(x, t, e) }
+    val pT: Parser[T] = "Int" ^^^ alg.tyInt()
+  }
+
+  trait NewAlg[E, T] extends ExtAlg[E, T] {
+    def num(x: Int): E
+  }
+
+  trait NewPrint extends NewAlg[String, String] with Print {
+    def num(x: Int): String = x.toString
+  }
+
+  trait NewParser[E, T] extends ExtParser[E, T] {
+    override val alg: NewAlg[E, T]
+    override lazy val pLam: Parser[E] = new BaseParser[E] {
+      override val alg = NewParser.this.alg
+      override val pE: Parser[E] = NewParser.this.pE
+    }.pLam
+    val pNum: Parser[E] = numericLit ^^ { x => alg.num(x.toInt) }
+    override val pE: Parser[E] = pLam ||| pVar ||| pNum
+  }
+
+  def test2(): Unit = {
+    val p = new NewParser[String, String] {
+      override val alg = new NewPrint {}
+    }.pE
+    println(parse(p)("\\x.3"))
+  }
+
+  def main(args: Array[String]): Unit = {
+    test2()
+  }
+
 /*
 //BEGIN_BASEPARSER_UNTYPEDLAM
 trait BaseParser[E] {
@@ -18,7 +87,6 @@ trait ExtParser[E, T] extends BaseParser[E] {
   override val alg: ...
   override val pLam: Parser[E] = ("\\" ~> ident) ~ (":" ~> pT) ~ ("." ~> pE) ^^ ...
   val pT: Parser[T] = ...
-  override val pE: Parser[E] = ...
 }
 //END_EXTPARSER_TYPEDLAM
 
@@ -26,13 +94,14 @@ trait ExtParser[E, T] extends BaseParser[E] {
 trait NewParser[E, T] extends ExtParser[E, T] {
   ...
   override val alg: ...
-  val tempAlg = alg
-  override val pLam = new BaseParser[E] { override val alg = tempAlg }.pLam
+  override lazy val pLam: Parser[E] = new BaseParser[E] {
+    override val alg = NewParser.this.alg
+    override val pE: Parser[E] = NewParser.this.pE
+  }.pLam
   ...
 }
 //END_NEWPARSER_UNTYPEDLAM
  */
-
 
 }
 
