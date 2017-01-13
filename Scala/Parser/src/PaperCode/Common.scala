40ff3330c8866dfbf15f828f3b03617a2a23af0b
Huang Li <lihuanglx@gmail.com>
2017-01-13 15:25:40 +0800

    Done 6.1

--- Scala/Parser/src/PaperCode/Common.scala
+++ Scala/Parser/src/PaperCode/Common.scala
@@ -8,6 +8,9 @@ trait Common extends StandardTokenParsers with PackratParsers {
 
   type Parser[E] = PackratParser[E]
 
+  val lcid: Parser[String] = ident ^? { case id if id.charAt(0).isLower => id }
+  val ucid: Parser[String] = ident ^? { case id if id.charAt(0).isUpper => id }
+
   def parse[E](p: Parser[E]): String => E = in => {
     val t = phrase(p)(new lexical.Scanner(in))
     if (t.successful) t.get else scala.sys.error(t.toString)
